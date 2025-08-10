82b0e456f9f8ef5862cf357c5c3280b2b657d833
Miembro-C <mariomejia2505@gmail.com>
2025-08-10 14:32:34 -0600

    challenge done

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-hola
+# Amigo Secreto 🎁
+
+Aplicación web simple para agregar nombres de amigos y realizar un sorteo aleatorio que determine quién es el "amigo secreto".  
+Este proyecto forma parte del **Oracle ONE Challenge**.
+
+## 📸 Capturas de pantalla
+
+![Texto alternativo](assets/result.png)
+
+
+---
+
+## 📋 Descripción del desafío
+
+En este desafío desarrollé una aplicación que permite:
+- **Agregar nombres** a una lista mediante un campo de texto y un botón `Añadir`.
+- **Validar la entrada** mostrando una alerta si el campo está vacío.
+- **Visualizar la lista** de nombres ingresados debajo del campo de entrada.
+- **Sortear un amigo** seleccionando aleatoriamente un nombre de la lista y mostrando el resultado en pantalla.
+
+---
+
+## 🛠️ Tecnologías utilizadas
+- HTML5
+- CSS3
+- JavaScript
+
+---
+
+## 🚀 Cómo ejecutar el proyecto
+1. Clona este repositorio:
+   ```bash
+   git clone https://github.com/MarioGhost25/challenge-amigo-secreto.git
--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let friends = [];
+
+function agregarAmigo() {
+    let inputFriend = document.getElementById('amigo');
+    if (inputFriend.value.length === 0) {
+        alert('Por favor, inserte un nombre.')
+    }
+    else {
+        friends.push(inputFriend.value);
+        inputFriend.value = '';
+        recorrerLista();
+    }
+
+}
+
+
+function recorrerLista() {
+    let ul = document.getElementById('listaAmigos');
+    ul.innerHTML = '';
+    for (let i = 0; i < friends.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = friends[i];
+        ul.appendChild(li);
+
+    }
+}
+
+
+function sortearAmigo() {
+
+    if (friends.length !== 0) {
+        let select = Math.floor(Math.random() * friends.length);
+        select = friends[select];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = select;
+
+    } else {
+        alert('Agregue los nombres para poder sortear.')
+    }
+
+}
+
